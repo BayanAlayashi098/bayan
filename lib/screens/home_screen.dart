ddac1cef1cf16d2398f5da335d7e54549d38dde4
BayanAlayashi098 <hadeelalsamy100@gmail.com>
2026-09-15 09:35:26 +0300

    FFF

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -43,7 +43,7 @@ class HomeScreen extends StatelessWidget {
                   SizedBox(width: 16),
                   Expanded(
                     child: Text(
-                        'bayan suspicious links with VirusTotal and check password strength locally.'),
+                        'DDDDDDDDDDDD suspicious links with VirusTotal and check password strength locally.'),
                   ),
                 ],
               ),
